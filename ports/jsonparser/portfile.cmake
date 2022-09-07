ca3f3321061e53ab7fdc6b6993ee1d454652d758
neogulcity <neogulcity@gmail.com>
2022-09-07 10:10:14 +0900

    Update portfile.cmake

--- ports/jsonparser/portfile.cmake
+++ ports/jsonparser/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO neogulcity/jsonparser
-  REF v1.0.0
+  REF 1.0.0
   SHA512 3f7e08d99e803dacdf17116bbfb665eb285a46b9bb61e510087586e31cc31312ddd7d279c79b4eca7a2e38ab20a80a7467d059723184dc61c21a2c76e41a99ad
   HEAD_REF main
 )
